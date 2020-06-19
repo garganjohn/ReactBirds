265d3a7e8442c070ec4d937a31854d850112ac49
garganjohn <garganjohn@gmail.com>
2020-06-19 14:02:14 -0400

    some changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+const url =
+  "https://gist.githubusercontent.com/garganjohn/4e4b2045c7e938baf9291d934a56b1bf/raw/e7b9dacdbb94a831d53a3676cd42aaef1050ce09/AntarcticBirds.json";
+interface Bird {
+  family: string;
+  members: string[];
 }
+const App: React.FC = () => {
+  const [source, setSource] = useState();
+  const [description, setDescription] = useState();
+  const [birds, setBirds] = useState<Bird[]>();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setSource(data.source);
+        setDescription(data.description);
+        setBirds(data.birds);
+        setLoading(false);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+  return (
+    <>
+
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          {birds?.map((bird) => {
+            return(
+              <div>
+                <h3>{bird.family}</h3>
+                {bird.members.map(mem => {
+                  return <p>{mem}</p>
+                })}
+              </div>
+            )
+          })}
+        </>
+      )}
+    </>
+  );
+ 
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
+import * as React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+
+// const url = 'https://gist.githubusercontent.com/garganjohn/4e4b2045c7e938baf9291d934a56b1bf/raw/e7b9dacdbb94a831d53a3676cd42aaef1050ce09/AntarcticBirds.json'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+
